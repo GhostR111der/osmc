7be827b7a1694411040b1ea480cdb78d533b9408
GhostR111der <alekseizubkov@me.com>
2020-09-13 19:52:02 +0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+
 # exit on error
 set -e
+
 TARGET=${1:-hyperion}
 CFG="${2:-Release}"
 INST="$( [ "${3:-}" = "install" ] && echo true || echo false )"
+
 sudo apt-get update
 sudo apt-get install                 \
 	git                          \
@@ -25,16 +28,20 @@ sudo apt-get install                 \
 	libjpeg-dev                  \
 	libqt5sql5-sqlite            \
 	zlib1g-dev                   \
+
 if [ -e /dev/vc-cma -a -e /dev/vc-mem ]
 then
 	sudo apt-get install libraspberrypi-dev
 fi
+
 git clone --recursive https://github.com/hyperion-project/hyperion.ng.git ${TARGET}
+
 rm -rf $TARGET/build
 mkdir -p $TARGET/build
 cd $TARGET/build
 cmake -DCMAKE_BUILD_TYPE=$CFG ..
 make -j2 $(nproc)
+
 # optional: install into your system
 $INST && sudo make install/strip
 echo "to uninstall (not very well tested, please keep that in mind):"
